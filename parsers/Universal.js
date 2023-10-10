03c66d560e8f68da8842a7ef17c5d6dd2038e5b0
JohnFrankz <86246881+JohnFrankz@users.noreply.github.com>
2023-10-10 11:01:07 +0800

    add Universal

--- parsers/Universal.js
+++ parsers/Universal.js
@@ -6,13 +6,13 @@ module.exports.parse = (raw, { yaml, console }) => {
         return [...new Set(arrays.flat())];
     }
 
-    const baseProxy = ["HK", "TW", "SG", "JP", "US", "Asia",];
+    const baseProxy = ["HK", "TW", "SG", "JP", "US", "Other",];
     const MustProxy = ["PROXY", ...baseProxy];
     const DirectFirst = ["DIRECT", ...MustProxy];
     const ProxyFirst = ["PROXY", "DIRECT", ...baseProxy];
     const AI = combineAndDeduplicate(["US"], MustProxy);
     const HamiVideo = combineAndDeduplicate(["TW"], MustProxy);
-    const StarPlusLogin = combineAndDeduplicate(["US"], MustProxy);
+    const StarPlusLogin = combineAndDeduplicate(["Other"], MustProxy);
     const StarPlus = combineAndDeduplicate(["US"], MustProxy);
 
     const updatedRawObj = updateProxyGroups(rawObj, { MustProxy, DirectFirst, ProxyFirst, baseProxy, AI, HamiVideo, StarPlusLogin, StarPlus });
