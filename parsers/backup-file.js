6555aa113402e3d201eab8baea6d2b2a54ad774c
JohnFrankz <86246881+JohnFrankz@users.noreply.github.com>
2023-10-21 20:26:12 +0800

    Bug Fixed

--- parsers/backup-file.js
+++ parsers/backup-file.js
@@ -1,20 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const { exec } = require('child_process');
+const os = require('os');
+
 module.exports.parse = async (raw, {yaml}, { name}) => {
-    const fs = require('fs');
-    const path = require('path');
-    const { exec } = require('child_process');
+    const configFile = path.join(__dirname, 'config/config.json');
+    const config = JSON.parse(fs.readFileSync(configFile, 'utf-8'));
+    if (config.backDir === undefined || config.backDir === '') {
+        return raw;
+    }
+    const backupDir = config.backDir.replace('~', os.homedir());
+
+    // 检查备份目录是否存在，如果不存在则创建它
+    if (!fs.existsSync(backupDir)) {
+        fs.mkdirSync(backupDir, { recursive: true }); 
+    }
 
     // 1. 生成当前时间的字符串形式
     const currentDate = new Date();
     const timestamp = `${currentDate.getMonth() + 1}-${currentDate.getDate()}-${String(currentDate.getHours()).padStart(2, '0')}:${String(currentDate.getMinutes()).padStart(2, '0')}:${String(currentDate.getSeconds()).padStart(2, '0')}`;
 
     // 2. 创建文件名
-    const configFile = path.join(__dirname, 'config/config.json');
-    const config = JSON.parse(fs.readFileSync(configFile, 'utf-8'));
-    const backPath = path.join(config.backDir, `${name}_${timestamp}.yaml`);
+    const backPath = path.join(backupDir, `${name}_${timestamp}.yaml`);
     fs.writeFileSync(backPath, raw);
 
     // 3. 列出与特定文件名相关的备份文件并保留最新的3个
-    const backupDir = config.backDir;
     const backupFiles = fs.readdirSync(backupDir)
         .filter(file => file.startsWith(`${name}_`) && file.endsWith('.yaml'))  // 过滤出与特定文件名相关的备份文件
         .sort((a, b) => {
